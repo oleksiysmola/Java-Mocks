b8b2396322d637ab1abb5eb57d6de4b4803864d9
Oleksiy Smola <oleksiy.smola@gmail.com>
2022-02-09 15:04:36 +0000

    added PersonService class

--- src/main/java/com/smola/week4/wednesday/person/PersonDAO.java
+++ src/main/java/com/smola/week4/wednesday/person/PersonDAO.java
@@ -4,7 +4,10 @@ import java.util.List;
 
 public interface PersonDAO {
     void savePerson(Person person);
+
     void deletePerson(Integer id);
+
     List<Person> getPeople();
+
     Person getPersonById(Integer id);
 }
